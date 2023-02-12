c22160109266e45a9fa759347cf8b9531d7c3b96
Sarera <Sarerabib@gmail.com>
2023-02-12 23:15:48 +0200

    bot_base

---
+++ index.js
@@ -0,0 +1,27 @@
+const { Client, GatewayIntentBits } = require('discord.js')
+const mySecret = 'MTAxNzE1NTcyMjcwMzgxODg5NA.GqDyH_._76d4oOl-yYelIgEv2CFVSDP9GXkLvHwPZMKN8';
+const gamedig = require('gamedig');
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+    ]
+})
+
+async function online() {
+    gamedig.query({type: 'valheim', host: `62.122.215.164`, port: "9876", requestRules: true
+    }).then((state) => {
+        client.user.setActivity(`Онлайн: ${state.raw.numplayers}/${state.maxplayers}`, {type: 0});
+    }).catch((error) => {
+        client.user.setActivity(`Сервер offline`, {type: 0});
+        console.log(error)
+    });
+}
+
+client.on("ready", () => {
+    console.log(`Bot_UP!`);
+    online()
+    setInterval(online, 50000)
+});
+
+client.login(mySecret)
