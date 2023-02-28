2267bcd81c42ef61ca34c4a582833d56d365cba6
Sarera <Sarerabib@gmail.com>
2023-03-01 00:32:39 +0200

    updatebot

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const { Client, GatewayIntentBits } = require('discord.js')
 
-let config = require('./token.json');
+let config = require('./config.json');
 let token = config.token;
 let prefix = config.prefix;
+let game = config.game;
+let ip = config.ip;
+let port = config.port;
 
 console.log(prefix)
 
@@ -11,12 +14,17 @@ const client = new Client({
         GatewayIntentBits.Guilds,
     ]
 })
+
 const gamedig = require('gamedig');
 
 async function online() {
-    gamedig.query({type: 'valheim', host: `62.122.215.164`, port: "9876", requestRules: true
-    }).then((state) => {
-        client.user.setActivity(`Онлайн: ${state.raw.numplayers}/${state.maxplayers}`, {type: 0});
+    gamedig.query({type: `${game}`, host:  "62.122.215.164", port:  "9876", requestRules: true
+     }).then((state) => {
+        console.log(state)
+
+        client.user.setActivity(
+            `Онлайн [${state.raw.numplayers}/${state.maxplayers}] `, {type: 0});
+
     }).catch((error) => {
         client.user.setActivity(`Сервер offline`, {type: 0});
         console.log(error)
@@ -26,8 +34,13 @@ async function online() {
 client.on("ready", () => {
     console.log(`Bot_UP!`);
     online()
-    setInterval(online, 50000)
+    setInterval(online, 10000)
 });
+
+
+1
+
+
 
 
 
---
+++ start.bat
@@ -0,0 +1,6 @@
+@echo off
+
+start npm i discord.js gamedig
+start node index.js
+
+echo Bot started!
